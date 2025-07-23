7a3aec1f3d4e67efcf30d835ab0583997dd8dca5
hasithasandun <hasiofficial2002@gmail.com>
2025-07-23 14:02:11 +0530

    Update BarcodeService to improve method consistency and readability

---
+++ backend/inventoryservice/src/main/java/com/InventoryMangementSystem/inventoryservice/dto/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.InventoryMangementSystem.inventoryservice.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+    private String name;
+}
