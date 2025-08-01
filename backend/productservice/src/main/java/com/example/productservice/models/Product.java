6931ae9136fc9c7e246bd75b43d15729fa956efa
hasithasandun <hasiofficial2002@gmail.com>
2025-08-01 20:54:02 +0530

    Add hello world print statements in ProductserviceApplication

---
+++ backend/productservice/src/main/java/com/example/productservice/models/Product.java
@@ -0,0 +1,26 @@
+package com.example.productservice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    private double price;
+}
