ee4cc2e3d1b4bce94461c14271777082d650638e
hasithasandun <hasiofficial2002@gmail.com>
2025-08-02 08:31:56 +0530

    test

--- backend/inventoryservice/src/main/java/com/InventoryMangementSystem/inventoryservice/InventoryserviceApplication.java
+++ backend/inventoryservice/src/main/java/com/InventoryMangementSystem/inventoryservice/InventoryserviceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryserviceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("kjbjkbkvbkhl");
 		SpringApplication.run(InventoryserviceApplication.class, args);
 
 	}
