96f253a98b698b37618fefb45a0956507dee322d
Kasun333 <kumarajwkd.22@uom.lk>
2025-08-11 22:23:25 +0530

    Get all confirmed orders and orders by I as well

---
+++ backend/Orderservice/src/main/java/com/Orderservice/Orderservice/entity/Product.java
@@ -0,0 +1,43 @@
+package com.Orderservice.Orderservice.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "price", precision = 10, scale = 2)
+    private BigDecimal price;
+    
+    @Column(name = "stock")
+    private Integer stockQuantity;
+    
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "image_url") // Add this field
+    private String imageUrl;
+}
