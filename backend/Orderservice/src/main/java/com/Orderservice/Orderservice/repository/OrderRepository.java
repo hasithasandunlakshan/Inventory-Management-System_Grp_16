6ff4a0d5ac8c2ef47de1238659b2469c86965085
Kasun333 <kumarajwkd.22@uom.lk>
2025-08-11 22:23:25 +0530

    Get all confirmed orders and orders by I as well

--- backend/Orderservice/src/main/java/com/Orderservice/Orderservice/repository/OrderRepository.java
+++ backend/Orderservice/src/main/java/com/Orderservice/Orderservice/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package com.Orderservice.Orderservice.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Orderservice.Orderservice.entity.Order;
@@ -12,4 +13,10 @@ import com.Orderservice.Orderservice.enums.OrderStatus;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByCustomerId(Long customerId);
     List<Order> findByStatus(OrderStatus status);
+    @Query("SELECT o FROM Order o JOIN FETCH o.orderItems")
+    List<Order> findAllWithOrderItems();
+
+     @Query("SELECT o FROM Order o JOIN FETCH o.orderItems WHERE o.status = 'CONFIRMED'")
+    List<Order> findAllConfirmedOrdersWithItems();
+    
 }
