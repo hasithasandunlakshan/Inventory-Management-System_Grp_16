c9d7e9bc5abc21b0626de04a0ecb6c905e628629
lihini-2002 <lihinikaduarachchilagh.22@uom.lk>
2025-08-23 20:45:09 +0530

    supplier database tables

--- backend/supplierservice/src/main/java/com/supplierservice/supplierservice/SupplierserviceApplication.java
+++ backend/supplierservice/src/main/java/com/supplierservice/supplierservice/SupplierserviceApplication.java
@@ -2,17 +2,17 @@ package com.supplierservice.supplierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SupplierserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SupplierserviceApplication.class, args);
-		System.out.println("Supplier Service is running...");
-		System.out.println("Access the API at: http://localhost:8080/supplier");
-		
+        System.out.println("Supplier Service is running...");
+        System.out.println("Access the API at: http://localhost:8080/supplier");
+
     }
 
 }
-
-
