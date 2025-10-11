19a0398a9acb31e8f05595b07a79393eca57dd62
lihini-2002 <lihinikaduarachchilagh.22@uom.lk>
2025-10-11 16:00:14 +0530

    testing

--- backend/supplierservice/src/main/java/com/supplierservice/supplierservice/repository/TestSupplierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.supplierservice.supplierservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.supplierservice.supplierservice.models.TestSupplier;
-
-public interface TestSupplierRepository extends JpaRepository<TestSupplier, Long> {
-}
